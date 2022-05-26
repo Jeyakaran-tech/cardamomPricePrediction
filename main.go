14b8bfab592ad57e70534ecd4726dda93748264a
Jeyakaran Karnan <jeyakaran30@gmail.com>
2022-05-26 23:37:04 +1000

    code modified

--- main.go
+++ main.go
@@ -24,78 +24,30 @@ func main() {
 	logger.Print("starting server...")
 	http.HandleFunc("/cardamom", handler)
 	appengine.Main()
-	// Determine port for HTTP service.
-	// port := os.Getenv("PORT")
-	// if port == "" {
-	// 	port = "8080"
-	// 	logger.Printf("defaulting to port %s", port)
-	// }
-
-	// // Start HTTP server.
-	// logger.Printf("listening on port %s", port)
-	// if err := http.ListenAndServe(":"+port, nil); err != nil {
-	// 	logger.Fatal(err)
-	// }
 
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 
-	ctx := appengine.NewContext(r)
-
-	//[START get_default_bucket]
-	// Use `dev_appserver.py --default_gcs_bucket_name GCS_BUCKET_NAME`
-	// when running locally.
-	// bucket, err := file.DefaultBucketName(ctx)
-	// if err != nil {
-	// 	log.Errorf(ctx, "failed to get default GCS bucket name: %v", err)
-	// }
-	//[END get_default_bucket]
 	bucket := "development-cardamomprice"
+	fileName := "cardamom-jk-go"
+	buf := &bytes.Buffer{}
+
+	var writer io.Writer
+	var prices []Price
+	c := colly.NewCollector()
+
+	ctx := context.Background()
 	client, err := storage.NewClient(ctx)
 	if err != nil {
 		log.Errorf(ctx, "failed to create client: %v", err)
 		return
 	}
 	defer client.Close()
+	fmt.Fprintf(writer, "Creating file /%v/%v\n", bucket, fileName)
+	wc := client.Bucket(bucket).Object(fileName).NewWriter(ctx)
 
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintf(w, "Demo GCS Application running from Version: %v\n", appengine.VersionID(ctx))
-	fmt.Fprintf(w, "Using bucket name: %v\n\n", bucket)
-
-	buf := &bytes.Buffer{}
-	d := &demo{
-		w:          buf,
-		ctx:        ctx,
-		client:     client,
-		bucket:     client.Bucket(bucket),
-		bucketName: bucket,
-	}
-
-	n := "cardamom-jk-go"
-	d.createFile(n)
-
-	if d.failed {
-		w.WriteHeader(http.StatusInternalServerError)
-		buf.WriteTo(w)
-		fmt.Fprintf(w, "\nfailed.\n")
-	} else {
-		w.WriteHeader(http.StatusOK)
-		buf.WriteTo(w)
-		fmt.Fprintf(w, "\nsucceeded.\n")
-	}
-
-}
-
-func (d *demo) createFile(fileName string) {
-	var prices []Price
-	m := make(map[string]string)
-	c := colly.NewCollector()
-	fmt.Fprintf(d.w, "Creating file /%v/%v\n", d.bucketName, fileName)
-
-	wc := d.bucket.Object(fileName).NewWriter(d.ctx)
-	wc.ContentType = "application/json"
-
+	//PROGRAMMING_LOGIC_FOR_DATA_EXTRACTION
 	for i := 0; i < 10; i++ {
 
 		c.OnHTML("div.tabstable tbody", func(e *colly.HTMLElement) {
@@ -131,29 +83,36 @@ func (d *demo) createFile(fileName string) {
 	cardamom := Cardamom{
 		Prices: &prices,
 	}
-	b, err := json.Marshal(cardamom)
+	byteResponse, err := json.Marshal(cardamom)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	err1 := json.Unmarshal(b, &m)
-	if err1 != nil {
-		logger.Fatal()
-	}
-	wc.Metadata = m
+
+	//PROGRAMMING_LOGIC_FINISHED
+	wc.ContentType = "application/json"
+	io.Copy(wc, bytes.NewReader(byteResponse))
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	fmt.Fprintf(w, "Demo GCS Application running from Version: %v\n", appengine.VersionID(ctx))
+	fmt.Fprintf(w, "Using bucket name: %v\n\n", bucket)
+	w.WriteHeader(http.StatusOK)
+	buf.WriteTo(w)
+	fmt.Fprintf(w, "\nsucceeded.\n")
 
 	if _, err := wc.Write([]byte("abcde\n")); err != nil {
-		d.errorf("createFile: unable to write data to bucket %q, file %q: %v", d.bucketName, fileName, err)
+		log.Errorf(ctx, "createFile: unable to write data to bucket %q, file %q: %v", bucket, fileName, err)
 		return
 	}
 	if _, err := wc.Write([]byte(strings.Repeat("f", 1024*4) + "\n")); err != nil {
-		d.errorf("createFile: unable to write data to bucket %q, file %q: %v", d.bucketName, fileName, err)
+		log.Errorf(ctx, "createFile: unable to write data to bucket %q, file %q: %v", bucket, fileName, err)
 		return
 	}
 	if err := wc.Close(); err != nil {
-		d.errorf("createFile: unable to close bucket %q, file %q: %v", d.bucketName, fileName, err)
+		log.Errorf(ctx, "createFile: unable to close bucket %q, file %q: %v", bucket, fileName, err)
 		return
 	}
+
 }
 
 type Cardamom struct {
@@ -166,21 +125,4 @@ type Price struct {
 	Market string `json:"market,omitempty"`
 	Type   string `json:"type,omitempty"`
 	Price  string `json:"price,omitempty"`
-}
-
-type demo struct {
-	client     *storage.Client
-	bucketName string
-	bucket     *storage.BucketHandle
-
-	w   io.Writer
-	ctx context.Context
-	// failed indicates that one or more of the demo steps failed.
-	failed bool
-}
-
-func (d *demo) errorf(format string, args ...interface{}) {
-	d.failed = true
-	fmt.Fprintln(d.w, fmt.Sprintf(format, args...))
-	log.Errorf(d.ctx, format, args...)
 }
