22df7a33bcc49978297540d0e34ea178835322c8
Jeyakaran Karnan <jeyakaran30@gmail.com>
2023-01-22 18:24:09 +1100

    modified code

--- cardamom.py
+++ cardamom.py
@@ -11,12 +11,19 @@ soup = BeautifulSoup(page.content, "html.parser")
 numberOfPagesString = soup.find("div", class_="text-warning").text.strip().split(" ")
 totalNumberOfPages = int(numberOfPagesString[3])
 
-for x in range(1, 3):
+f = open('./data.csv', 'w')
+writer = csv.writer(f)
+header = ['SNO', 'Date', 'Market', 'Type', 'Price']
+writer.writerow(header)
+
+for x in range(1, totalNumberOfPages):
     page = requests.get(URL+f"?page={x}")
     soup = BeautifulSoup(page.content, "html.parser")
 
     for rows in soup.find_all('tr')[5:]:
+        data = []
         for cols in rows.find_all('td'):
-            print(cols.text.strip(), ",")
-        print("\n")
-       
+            data.append(cols.text.strip())
+        writer.writerow(data)
+
+f.close()
